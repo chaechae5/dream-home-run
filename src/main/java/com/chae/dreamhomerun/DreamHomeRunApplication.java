b8e29dc9c0feb5f1085b2d10de7c6947d78428b8
eunchae oh <eunchae1083@gmail.com>
2024-10-06 17:49:09 +0900

    first commit

---
+++ src/main/java/com/chae/dreamhomerun/DreamHomeRunApplication.java
@@ -0,0 +1,13 @@
+package com.chae.dreamhomerun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamHomeRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamHomeRunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chae/dreamhomerun/DreamHomeRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chae.dreamhomerun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamHomeRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
